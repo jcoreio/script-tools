307ebddaa3245226ce439bea3479d75cb6800c69
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2018-11-26 19:35:08 -0600

    fix: disable strict host key checking on execRemote ssh logins

--- src/execRemote.js
+++ src/execRemote.js
@@ -3,10 +3,15 @@
 import {spawn} from './spawn'
 
 // The -t -t option causes the remote process to be killed when this process is killed: https://unix.stackexchange.com/questions/103699/kill-process-spawned-by-ssh-when-ssh-dies
-export const execRemote = ({host, user, keyFile, command, spawnOpts}: {
+export const execRemote = ({host, user, keyFile, command, strictHostKeyChecking, spawnOpts}: {
   host: string,
   user?: ?string,
   keyFile?: ?string,
   command: string,
+  strictHostKeyChecking?: ?boolean,
   spawnOpts?: ?Object,
-}) => spawn('ssh', ['-t', '-t', ...(user ? ['-l', user] : []), ...(keyFile ? ['-i', keyFile] : []), host, command], spawnOpts || {})
+}) => spawn('ssh', ['-t', '-t',
+  ...(strictHostKeyChecking ? [] : ['-o', 'StrictHostKeyChecking=no']),
+  ...(user ? ['-l', user] : []),
+  ...(keyFile ? ['-i', keyFile] : []),
+  host, command], spawnOpts || {})
