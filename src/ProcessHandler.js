5c639b1ee149a72caed3e1257dd05ca0e4227b22
Jason McLaurin <jrmclaurin@users.noreply.github.com>
2018-11-27 18:57:39 -0600

    feat(process-handler): add support for calling handler.exec(), add printCommands constructor option

--- src/ProcessHandler.js
+++ src/ProcessHandler.js
@@ -1,12 +1,13 @@
 // @flow
 
 import type { ChildProcess } from 'child_process'
-import type { ChildProcessPromise } from 'promisify-child-process'
+import { exec as baseExec, type ChildProcessPromise } from 'promisify-child-process'
+import { VError } from 'verror'
 
 import nodeCleanup from 'node-cleanup'
 
 import { spawn as baseSpawn, type SpawnOpts } from './spawn'
-import { execRemote as baseExecRemote, type ExecRemoteArgs} from './execRemote'
+import { execRemote as baseExecRemote, type ExecRemoteArgs } from './execRemote'
 
 export class ProcessHandler {
 
@@ -14,20 +15,41 @@ export class ProcessHandler {
   _cleanupHandlerInstalled: boolean = false
   _curId: number = 0
   _maxProcesses: number
+  _printCommands: boolean
 
-  constructor({maxProcesses}: {maxProcesses?: ?number} = {}) {
+  constructor({maxProcesses, printCommands}: {maxProcesses?: ?number, printCommands?: ?boolean} = {}) {
     this._maxProcesses = maxProcesses || 100
+    this._printCommands = !!printCommands
+  }
+
+  exec(command: string, options?: child_process$execOpts): ChildProcessPromise {
+    this._maybePrintCommand(`+ ${command}`)
+    const child = baseExec(command, options)
+    this.killOnExit(child)
+    child.catch((err: Error) => {
+      throw new VError(err, `exec failed: ${command}`)
+    })
+    return child
   }
 
   execRemote(args: ExecRemoteArgs): ChildProcessPromise {
+    this._maybePrintCommand(`+ host: ${args.host} command: ${args.command}`)
     const child = baseExecRemote(args)
     this.killOnExit(child)
+    child.catch((err: Error) => {
+      throw new VError(err, `execRemote on host ${args.host} failed: ${args.command}`)
+    })
     return child
   }
 
   spawn(command: string, args: Array<any> | Object | void, options: SpawnOpts = {}): ChildProcessPromise {
+    const commandAndArgs = `${command}${args && args.length ? ' ' + args.join(' ') : ''}`
+    this._maybePrintCommand(`+ ${commandAndArgs}`)
     const child = baseSpawn(command, args, options)
     this.killOnExit(child)
+    child.catch((err: Error) => {
+      throw new VError(err, `spawn failed: ${commandAndArgs}`)
+    })
     return child
   }
 
@@ -51,6 +73,11 @@ export class ProcessHandler {
       child.kill()
     }
     this._runningProcesses.clear()
+  }
+
+  _maybePrintCommand(...args: Array<any>) {
+    if (this._printCommands)
+      console.log(...args)
   }
 
 }
